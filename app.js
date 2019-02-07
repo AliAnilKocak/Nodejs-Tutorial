c6f589609ac807842d8757cfec2d5c351ccd90f3
Ali Anıl Koçak <alianilkocak1@gmail.com>
2019-02-07 19:34:47 +0300

    Code Formatted and Cleanup

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const tutorial = require('./tutorial.js');
-console.log("Ben baş kaldırıyorum ifadelerim özgür suratımda. Hürriyetimi görüyorum tek başımayım ama çokum bu kavgada.");
 console.log(tutorial.div(1, 5));
+console.log(tutorial.sum(4,5));
 console.log(new tutorial.someClass());
-
 console.log(new tutorial.someClass().inClass(25, 5));
