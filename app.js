09599d5aefd312069101d7d5d82885679430d463
Ali Anıl Koçak <alianilkocak1@gmail.com>
2019-02-07 19:19:12 +0300

    How to module export another .js files for functions, variables,classses

---
+++ app.js
@@ -0,0 +1,6 @@
+const tutorial = require('./tutorial.js');
+console.log("Ben baş kaldırıyorum ifadelerim özgür suratımda. Hürriyetimi görüyorum tek başımayım ama çokum bu kavgada.");
+console.log(tutorial.div(1, 5));
+console.log(new tutorial.someClass());
+
+console.log(new tutorial.someClass().inClass(25, 5));
---
+++ tutorial.js
@@ -0,0 +1,19 @@
+const sum = (number1, number2) => number1 + number2;
+const div = (number1, number2) => number1 - number2;
+const someClass = class someClass {
+
+    constructor() {
+        console.log("Some class created.");
+    }
+
+    inClass(n1, n2) {
+        let result = 0;
+        let name = "anil";
+        for (let i = 0; i < sum(div(n1, n2), sum(n1, n2)); i = i + 3) {
+            result += div(n1, n2);
+        }
+        return result;
+    }
+};
+
+module.exports = {sum: sum, div: div, someClass: someClass};
