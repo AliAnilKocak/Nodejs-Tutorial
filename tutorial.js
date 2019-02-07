13d1814e752535fb9ade45aaf4887318d9d10074
Ali Anıl Koçak <alianilkocak1@gmail.com>
2019-02-07 19:42:45 +0300

    Code Formatted and Cleanup

--- tutorial.js
+++ tutorial.js
@@ -8,7 +8,6 @@ const someClass = class someClass {
 
     inClass(n1, n2) {
         let result = 0;
-        let name = "anil";
         for (let i = 0; i < sum(div(n1, n2), sum(n1, n2)); i = i + 3) {
             result += div(n1, n2);
         }
