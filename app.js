7dc8f8bd616afe709e1bd6873b2b4fd6e52d019c
Ali Anıl Koçak <alianilkocak1@gmail.com>
2019-02-08 14:54:51 +0300

    Readline module with EventEmitter

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ console.log(new tutorial.someClass());
 console.log(new tutorial.someClass().inClass(25, 5));//Export module
 */
 
+/*
 
 const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
@@ -38,4 +39,37 @@ anil.on('name', ()=>{
     console.log("My Name Is "+anil.name);
 });
 
-anil.emit('name');
+anil.emit('name');*/
+
+const readLine = require('readline');
+const rl = readLine.createInterface({input: process.stdin, output: process.stdout});
+
+
+let num = Math.floor((Math.random() * 20) + 1);
+let num1 = Math.floor((Math.random() * 20) + 1);
+let answer = num + num1;
+
+rl.question(`What's ${num}+${num1}=? \n`, (userInput) => {
+    if (userInput.trim() == answer) {
+       rl.close();
+    }else{
+        rl.setPrompt('Incorrect answer Please try again. \n');
+        rl.prompt(true);
+        rl.on("line", (userInput)=>{
+           if(userInput.trim() == answer){
+               rl.close();
+           }else{
+               rl.setPrompt(`Your answer:  ${ answer } is incorrect. Please try again. \n`);
+               rl.prompt();
+           }
+        });
+
+    }
+});
+
+rl.on('close',()=>{
+    console.log('correct!!');
+})
+
+
+
