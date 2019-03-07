caa1dc7d707beb70fffb28bd0512216868121c89
AliAnilKocak <alianilkocak@gmail.com>
2019-03-07 12:05:39 +0300

    EJS Templates With Express

--- app.js
+++ app.js
@@ -310,8 +310,7 @@ app.post('/', (req, res) => {
 app.listen(3000);
 */
 
-
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 const Joi = require('joi');
 const bodyParser = require('body-parser');
@@ -345,12 +344,24 @@ app.post('/',(req,res)=>{
 });
 
 
+app.listen(3000);*/
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.set('view engine','ejs');
+app.get('/:userQuery',(req,res)=>{
+    res.render('index',{data : {userQuery: req.params.userQuery,
+            searchResults : ['book1','book2','book3'],
+            loggedIn : true,
+            username : 'lkjslkjdf'}});
+});
+
+
+
 app.listen(3000);
-
-
-
-
-
 
 
 
