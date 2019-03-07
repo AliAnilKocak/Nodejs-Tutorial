82d4eaed862877076e82f28c983da4127f5f68ed
AliAnilKocak <alianilkocak@gmail.com>
2019-03-07 11:44:48 +0300

    User Input Validation With Express And JOI

--- app.js
+++ app.js
@@ -283,6 +283,7 @@ app.get('/',(req,res)=>{
 
 }).listen(3000);*/
 
+/*
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -307,13 +308,44 @@ app.post('/', (req, res) => {
 });
 
 app.listen(3000);
+*/
 
 
+const express = require('express');
+const path = require('path');
+const Joi = require('joi');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use('/public', express.static(__dirname + '/static/css'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'static','index.html'));
+});
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    const schema = Joi.object().keys({
+        email : Joi.string().trim().email().required(),
+        password: Joi.string().min(5).max(10).required()
+    });
+    Joi.validate(req.body,schema,(err,result)=>{
+        if(err){
+            console.log(err);
+            res.send('an error has occurred');
+        }
+        else{
+            console.log(result);
+            res.send('successfully posted data');
+        }
+
+    })
+
+});
 
 
-
-
-
+app.listen(3000);
 
 
 
